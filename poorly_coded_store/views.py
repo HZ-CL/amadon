1a6ecedf7898d2ed434cff763a173e30b0342235
Hector Zambrano <hzambranoleal@gmail.com>
2021-03-18 20:45:13 -0300

    amadon

--- poorly_coded_store/views.py
+++ poorly_coded_store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Order, Product
 
 def index(request):
@@ -9,8 +9,35 @@ def index(request):
 
 def checkout(request):
     quantity_from_form = int(request.POST["quantity"])
-    price_from_form = float(request.POST["price"])
-    total_charge = quantity_from_form * price_from_form
+    #price_from_form = float(request.POST["price"])
+#forma diferente de obtener el precio
+    produc_id=int(request.POST["producto_id"])
+    producto = Product.objects.get(id=produc_id)
+    price=producto.price
+
+    total_charge = quantity_from_form * price
     print("Charging credit card...")
     Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    return render(request, "store/checkout.html")
+
+
+    return redirect('/success')
+    #return render(request, "store/checkout.html", context)
+
+def success(request):
+    order_all = Order.objects.all()
+    gasto_total = 0
+    for pedido in order_all:
+        gasto_total += pedido.total_price
+
+    item_all = 0
+    for items in order_all:
+        item_all += items.quantity_ordered
+
+
+    orden = Order.objects.last()
+    context = {
+        'last_orden': orden,
+        'item_all':item_all,
+        'gasto_total': gasto_total,
+    }
+    return render(request, "store/checkout.html", context)
